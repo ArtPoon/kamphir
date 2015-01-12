d8fc2b2fd4431422609f8e6fe0b1e672ba0eed8d
ArtPoon <artpoon@gmail.com>
2015-01-12 11:25:05 -0800

    json script for acute-chronic sim

---
+++ projects/hivepi/scripts/view-logs.R
@@ -0,0 +1,5 @@
+n100 <- read.table('~/git/kamphir/projects/hivepi/data/SIRTree.n100.RLRootToTip.timetree.log', header=T, sep='\t')
+
+n300 <- read.table('~/git/kamphir/projects/hivepi/data/SIRTree.n300.RLRootToTip.timetree.log.2', header=T, sep='\t')
+
+n1000 <- read.table('~/git/kamphir/projects/hivepi/data/SIRTree.n1000.RLRootToTip.timetree.log', header=T, sep='\t')
