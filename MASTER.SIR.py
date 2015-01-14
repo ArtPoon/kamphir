7bd986f6903da23b901e8c3ec6d136f02e661fd5
ArtPoon <artpoon@gmail.com>
2015-01-13 23:24:54 -0800

    resolved slow processing problem for large trees

--- MASTER.SIR.py
+++ MASTER.SIR.py
@@ -9,6 +9,7 @@ from Bio import Phylo
 from random import sample
 import subprocess
 import time
+from datetime import datetime
 
 # TODO: allow user to set time limit and step for MASTER
 
@@ -16,7 +17,8 @@ time_limit = 60  # seconds - when do we reduce the number of tips
 time_step = 10 # seconds - how often we check the file for completion
 
 
-jarfile = '/Users/art/src/MASTER-2.0.0/dist/MASTER-2.0.0/MASTER-2.0.0.jar'
+#jarfile = '/Users/art/src/MASTER-2.0.0/dist/MASTER-2.0.0/MASTER-2.0.0.jar'
+jarfile = '/Users/art/src/MASTER-2.0.0/MASTER-2.0.0.jar'
 #jarfile = '/home/art/src/MASTER-2.0.0/MASTER-2.0.0.jar'
 FNULL = open(os.devnull, 'w')
 
@@ -110,6 +112,7 @@ handle.close()
 handle = open(tipfile, 'rU')
 context['ntips'] = len(handle.readlines())
 handle.close()
+context['ntips'] = int(round(context['ntips'] * 0.8))
 
 # populate template from context
 handle = open(tmpfile, 'w')
@@ -121,8 +124,10 @@ if os.path.exists(outfile):
     os.remove(outfile)
 
 # call MASTER
+#print '[%s] calling master2' % datetime.now().isoformat()
+
 #os.system('master2 %s > /dev/null' % tmpfile)
-p = subprocess.Popen(['java', '-jar', jarfile, tmpfile],
+p = subprocess.Popen(['java', '-Xms512m', '-Xmx1024m', '-jar', jarfile, tmpfile],
                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # check if outfile has expected number of lines
@@ -147,21 +152,34 @@ while 1:
             print 'ERROR: ntips cannot be less than 2'
             sys.exit(1)
 
+        #print '[%s] reached time limit - reduced ntips to %d' % (datetime.now().isoformat(),
+        #    context['ntips'])
+        
         # update template
         handle = open(tmpfile, 'w')
         handle.write(template.render(context))
         handle.close()
 
-        p = subprocess.Popen(['java', '-jar', jarfile, tmpfile],
+        p = subprocess.Popen(['java', '-Xms512m', '-Xmx1024m', '-jar', jarfile, tmpfile],
                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         elapsed = 0  # reset timer
 
+p.kill()
+
+#print '[%s] reached target number of trees' % datetime.now().isoformat()
 
 # sample tips to enforce size of tree
 trees = Phylo.parse(outfile, 'newick')
 trees2 = []
-for tree in trees:
+while True:
+    try:
+        tree = trees.next()
+    except StopIteration:
+        break
+    except NewickError:
+        continue
+        
     tips = tree.get_terminals()
     try:
         tips2 = sample(tips, ntips)
@@ -174,5 +192,7 @@ for tree in trees:
             continue
         _ = tree.prune(tip)
     trees2.append(tree)
+
+#print '[%s] pruned trees' % datetime.now().isoformat()
 
 Phylo.write(trees2, outfile, 'newick')
