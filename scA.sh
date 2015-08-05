a34445f7503d49eed68fc708c18efb8b019f84d9
Rosemary <rmcclosk.math@gmail.com>
2015-08-05 16:17:11 -0700

    adding SI run script

---
+++ scA.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python kamphir.py SI /home/rmcclosk/Documents/kamphir/settings/example.SI.json /home/rmcclosk/Documents/pangea/data/February2015/Regional/hyphy/root2tip/150129_PANGEAsim_Regional_FirstObj_scA_SIMULATED_all.nwk scA.SI.log -delimiter _ -ncores 5 -nthreads 5 -nreps 5 -tscale 0.142857 -tol0 0.03 -mintol 0.01
